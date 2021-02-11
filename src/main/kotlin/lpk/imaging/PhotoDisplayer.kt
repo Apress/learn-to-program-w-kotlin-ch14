f553b06b62ee3e6b3ae31105eceffe6400d26998
Tim Lavers <laverstim@gmail.com>
2021-02-11 19:40:20 +1100

    Removed code

--- src/main/kotlin/lpk/imaging/PhotoDisplayer.kt
+++ src/main/kotlin/lpk/imaging/PhotoDisplayer.kt
@@ -1,14 +1,1 @@
 package lpk.imaging
-
-import java.nio.file.Paths
-
-fun main() {
-    PhotoDisplayer().doLaunch()
-}
-class PhotoDisplayer : PictureDisplayer() {
-    private val IMAGES = "src/main/resources/images/"
-    override fun createPicture() : Picture {
-        val file = Paths.get(IMAGES + "bay.png").toFile()
-        return loadPictureFromFile(file)
-    }
-}
